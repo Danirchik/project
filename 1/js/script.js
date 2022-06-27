4381c16d71ddfdf529b862dbd4a8663115c14d1a
Danir <Danir@mail.ru>
2022-06-28 00:01:19 +0300

    Последний хуй

--- 1/js/script.js
+++ 1/js/script.js
@@ -1,4 +1,26 @@
 "use strict";
 
- const user = "Timur";
- alert(`Привет, ${user}`); 
+
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movie: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const film1 = prompt('Один из последних просмотренных фильмов?', '');
+const ocenka1 = prompt('На сколько оцените его?', '');
+
+
+
+const film2 = prompt('Один из последних просмотренных фильмов?', '');
+const ocenka2 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movie[film1]=ocenka1;
+
+personalMovieDB.movie[film2]=ocenka2;
+
+console.log(personalMovieDB);
